58af7a608abc99c4beea08cb639631741dfc1522
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-03-24 14:18:14 -0400

    shoutem

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card } from '@shoutem/ui';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+		<Card>
+			<Text>Open up App.js to start working on your app!</Text>
+		</Card>
       </View>
     );
   }
